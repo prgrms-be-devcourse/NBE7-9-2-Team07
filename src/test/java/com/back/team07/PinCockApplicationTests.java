f6b197ddc8ec99a8fa14ffcaaa31869dcbcff1cf
Jung Yunseo <yunseo2488@naver.com>
2025-10-13 18:01:44 +0900

    chore: gradle kotlin 으로 변경 및 프로젝트 명 수정 - 테스트 파일

--- src/test/java/com/back/team07/PinCockApplicationTests.java
+++ src/test/java/com/back/team07/PinCockApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Nbe792Team07ApplicationTests {
+class PinCockApplicationTests {
 
     @Test
     void contextLoads() {
