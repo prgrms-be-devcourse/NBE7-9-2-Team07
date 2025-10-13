f14dc32db36aba1520462c1acfad0ee074bfb029
Jung Yunseo <yunseo2488@naver.com>
2025-10-13 09:03:20 +0900

    first commit

---
+++ src/main/java/com/back/team07/Nbe792Team07Application.java
@@ -0,0 +1,13 @@
+package com.back.team07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe792Team07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe792Team07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/team07/Nbe792Team07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.team07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe792Team07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
