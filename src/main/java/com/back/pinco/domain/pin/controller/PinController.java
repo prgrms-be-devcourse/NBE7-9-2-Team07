9bc8ef6fb2278ff10228c18dafc989ac479c1296
LeeHyun <nx2028@gmail.com>
2025-10-15 13:34:26 +0900

    re(pin): getAll api가 Dto를 반환하도록 수정

--- src/main/java/com/back/pinco/domain/pin/controller/PinController.java
+++ src/main/java/com/back/pinco/domain/pin/controller/PinController.java
@@ -1,15 +1,16 @@
 package com.back.pinco.domain.pin.controller;
 
+import com.back.pinco.domain.pin.dto.PinDto;
+import com.back.pinco.domain.pin.dto.PinListResponseDto;
 import com.back.pinco.domain.pin.entity.Pin;
 import com.back.pinco.domain.pin.service.PinService;
+import com.back.pinco.global.geometry.GeometryUtil;
 import com.back.pinco.global.rsData.RsData;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,61 +21,59 @@ public class PinController {
 
     @Autowired
     private PinService pinService;
-
-
+    @Autowired
+    private GeometryUtil geometryUtil;
 
     @GetMapping("")
     public RsData<List<Pin>> getRadiusPins(
             @NotNull
             @Min(0)
             @Max(20000)
-            @RequestParam
-            double radius,
-
+            @RequestParam double radius,
             @NotNull
             @Min(-90)
             @Max(90)
             @RequestParam double latitude,
-
             @NotNull
             @Min(-180)
             @Max(180)
             @RequestParam double longitude
     ) {
-        List<Pin> pins = pinService.findNearPins(radius,latitude,longitude);
-        if(pins.isEmpty()){
-            return new RsData<>(
-                    "204",
-            "조회된 값이 없습니다.",
-            null
-                    );
-        }
-        return new RsData<List<Pin>>(
-                "200",
-                "성공적으로 처리되었습니다",
-                pins
-        );
-
-    }
-
-    @GetMapping("/all")
-    public RsData<List<Pin>> getAll() {
-        List<Pin> pins = pinService.findAll();
-        if(pins.isEmpty()){
+        List<Pin> pins = pinService.findNearPins(radius, latitude, longitude);
+        if (pins.isEmpty()) {
             return new RsData<>(
                     "204",
                     "조회된 값이 없습니다.",
                     null
             );
         }
-        return new RsData<List<Pin>>(
+        return new RsData<>(
                 "200",
                 "성공적으로 처리되었습니다",
                 pins
         );
-
     }
 
+    @GetMapping("/all")
+    public RsData<PinListResponseDto> getAll() {
+        List<Pin> pins = pinService.findAll();
 
+        List<PinDto> pinDtos = pins.stream()
+                .map(pin -> new PinDto(pin, geometryUtil))
+                .toList();
+        PinListResponseDto pinListResponseDto = new PinListResponseDto(pinDtos);
 
+        if (pins.isEmpty()) {
+            return new RsData<>(
+                    "204",
+                    "조회된 값이 없습니다.",
+                    null
+            );
+        }
+        return new RsData<>(
+                "200",
+                "성공적으로 처리되었습니다",
+                pinListResponseDto
+        );
+    }
 }
