3ec1ac1c0c30859d84db7232a1a340b628539aef
kheeyoung <talent8661@gmail.com>
2025-10-15 12:10:14 +0900

    feat(post): post 단건, 다건 조회 구현

--- src/main/java/com/back/pinco/domain/post/dto/PostDto.java
+++ src/main/java/com/back/pinco/domain/post/dto/PostDto.java
@@ -1,4 +1,24 @@
 package com.back.pinco.domain.post.dto;
 
-public class PostDto {
+import com.back.pinco.domain.pin.entity.Pin;
+import com.back.pinco.domain.post.entity.Post;
+
+import java.time.LocalDateTime;
+
+public record PostDto(
+        Long id,
+        String content,
+        LocalDateTime createAt,
+        LocalDateTime modifiedAt,
+        Pin pin
+) {
+    public PostDto(Post post) {
+        this(
+                post.getId(),
+                post.getContent(),
+                post.getCreatedAt(),
+                post.getModifiedAt(),
+                post.getPin()
+        );
+    }
 }
