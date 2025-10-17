b9fb5c1661dc0376b4386e8d123f28becad17a30
77r77r <codingworld@naver.com>
2025-10-18 07:32:31 +0900

    docs(Bookmark) : 북마크 도메인에 주석 추가

--- src/main/java/com/back/pinco/domain/bookmark/dto/BookmarkRequestDto.java
+++ src/main/java/com/back/pinco/domain/bookmark/dto/BookmarkRequestDto.java
@@ -1,5 +1,10 @@
 package com.back.pinco.domain.bookmark.dto;
 
+/**
+ * 북마크 생성 요청 DTO
+ * @param userId 사용자 ID
+ * @param pinId 핀 ID
+ */
 public record BookmarkRequestDto(
         Long userId,
         Long pinId
