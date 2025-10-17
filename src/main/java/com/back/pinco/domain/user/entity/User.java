cbc2db26e2f8d95f1e50f7d063d13f6d7ca84b1d
Jung Yunseo <yunseo2488@naver.com>
2025-10-17 09:14:26 +0900

    fix : UserService 에서 Setter 사용 -> 엔티티 누락으로 인한 오류 해결

--- src/main/java/com/back/pinco/domain/user/entity/User.java
+++ src/main/java/com/back/pinco/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package com.back.pinco.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 @Table(
         name = "users",
         indexes = @Index(name = "idx_user_email", columnList = "email")
