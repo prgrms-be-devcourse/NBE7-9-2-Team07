5fbc9b588be9a3991e50f5f5e8170666bcb4eb6a
park jongsoo <jjangjongsoo@naver.com>
2025-10-17 14:29:26 +0900

    fix: 충돌 날것 같아서 PinDto main 버전으로 최신화

--- src/main/java/com/back/pinco/domain/pin/dto/PinDto.java
+++ src/main/java/com/back/pinco/domain/pin/dto/PinDto.java
@@ -1,20 +1,38 @@
-//package com.back.pinco.domain.pin.dto;
-//
-//import com.back.pinco.domain.pin.entity.Pin;
-//import java.time.LocalDateTime;
-//
-//public record PinDto(
-//        Long id,
-//        Double longitude,
-//        Double latitude,
-//        LocalDateTime createAt
-//) {
-//    public PinDto(Pin pin) {
-//        this(
-//                pin.getId(),
-//                pin.getPoint().getX(), // longitude
-                pin.getPoint().getY(), // latitude
-//                pin.getCreateAt()
-//        );
-//    }
-//}
+package com.back.pinco.domain.pin.dto;
+
+import com.back.pinco.domain.pin.entity.Pin;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.back.pinco.domain.tag.entity.PinTag;
+import com.back.pinco.domain.tag.entity.Tag;
+import com.back.pinco.domain.user.entity.User;
+import jakarta.persistence.*;
+import org.locationtech.jts.geom.Point;
+
+
+public record PinDto(
+        Long id,
+        Point point,
+        String content,
+        User user,
+        List<PinTag> pinTags,
+        int likeCount,
+        Boolean isPublic,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
+) {
+    public PinDto(Pin pin) {
+        this(
+                pin.getId(),
+                pin.getPoint(),
+                pin.getContent(),
+                pin.getUser(),
+                pin.getPinTags(),
+                pin.getLikeCount(),
+                pin.getIsPublic(),
+                pin.getCreatedAt(),
+                pin.getModifiedAt()
+        );
+    }
+}
