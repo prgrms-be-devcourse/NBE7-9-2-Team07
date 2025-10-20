de1265ea39e956dde3ef9354cbc95921849e5153
kheeyoung <talent8661@gmail.com>
2025-10-20 12:43:48 +0900

    feat(pin): initData 핀 생성 부분 CreatePinRequest로 변경

--- src/main/java/com/back/pinco/global/initData/InitData.java
+++ src/main/java/com/back/pinco/global/initData/InitData.java
@@ -2,7 +2,7 @@ package com.back.pinco.global.initData;
 
 import com.back.pinco.domain.bookmark.service.BookmarkService;
 import com.back.pinco.domain.likes.service.LikesService;
-import com.back.pinco.domain.pin.dto.PostPinReqbody;
+import com.back.pinco.domain.pin.dto.CreatePinRequest;
 import com.back.pinco.domain.pin.entity.Pin;
 import com.back.pinco.domain.pin.service.PinService;
 import com.back.pinco.domain.tag.entity.PinTag;
@@ -55,11 +55,11 @@ public class InitData {
         User user2 = userService.createUser("user2@example.com", "12341234", "유저2");
 
         // ✅ 시청 기준 반경 1km 이내 임의 좌표
-        Pin pinA = pinService.write(user1, new PostPinReqbody(baseLat + 0.0012, baseLng + 0.0015, "서울 시청 근처 카페 ☕"));
-        Pin pinB = pinService.write(user1, new PostPinReqbody(baseLat - 0.0008, baseLng + 0.0010, "덕수궁 돌담길 산책 중 🌳"));
-        Pin pinC = pinService.write(user1, new PostPinReqbody(baseLat + 0.0006, baseLng - 0.0013, "청계천 산책로 발견 👣"));
-        Pin pinD = pinService.write(user2, new PostPinReqbody(baseLat - 0.0005, baseLng - 0.0010, "광화문에서 커피 한 잔 ☕"));
-        Pin pinE = pinService.write(user2, new PostPinReqbody(baseLat + 0.0003, baseLng + 0.0002, "서울시청 옆 공원 벤치 휴식 🍃"));
+        Pin pinA = pinService.write(user1, new CreatePinRequest(baseLat + 0.0012, baseLng + 0.0015, "서울 시청 근처 카페 ☕"));
+        Pin pinB = pinService.write(user1, new CreatePinRequest(baseLat - 0.0008, baseLng + 0.0010, "덕수궁 돌담길 산책 중 🌳"));
+        Pin pinC = pinService.write(user1, new CreatePinRequest(baseLat + 0.0006, baseLng - 0.0013, "청계천 산책로 발견 👣"));
+        Pin pinD = pinService.write(user2, new CreatePinRequest(baseLat - 0.0005, baseLng - 0.0010, "광화문에서 커피 한 잔 ☕"));
+        Pin pinE = pinService.write(user2, new CreatePinRequest(baseLat + 0.0003, baseLng + 0.0002, "서울시청 옆 공원 벤치 휴식 🍃"));
 
 
         // ✅ 샘플 북마크 생성 (user1이 pinA, pinD 북마크 / user2가 pinB 북마크)
